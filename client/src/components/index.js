297f9f9c9f9f9b78104873ef13b999a361b887fa
dezade <zmsiddiqee2002@gmail.com>
2023-07-13 21:55:02 +0600

    Massive UI change

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navigation } from './Navigation';
-export { default as Section } from './Section';
-export { default as Welcome } from './Welcome';
-export { default as Footer } from './Footer';
-
